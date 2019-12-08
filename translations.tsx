5221cf1c82b73ac817d9c5e3bd8372a3fde00573
Lukas Mohrbacher <lukas.mohrbacher@diconium.com>
2019-12-08 23:23:18 +0100

    add pattern for consistent translation objects

--- translations.tsx
+++ translations.tsx
@@ -1,15 +1,15 @@
 import { Translation } from "./library";
 
-export const translations = {
-    en: {
-        hello: `hi`,
-        helloName: ({name}: {name: string}) => `hi ${name}`,
-        helloUrgent: ({name}: {name: string}) => <>hello<b>{name}</b></>
-    },
-    de: {
-        hello: `hallo`,
-        helloName: ({name}: {name: string}) => `hallo ${name}`,
-    }
+export const en = {
+    hello: `hi`,
+    helloName: ({name}: {name: string}) => `hi ${name}`,
+    helloBold: ({name}: {name: string}) => <>hello <b>{name}</b></>
 }
 
-export const { TranslationProvider, useTranslations, Translate } = new Translation(translations.en)
+export const de: typeof en = {
+    hello: `hallo`,
+    helloName: ({name}: {name: string}) => `hallo ${name}`,
+    helloBold: ({name}: {name: string}) => <>hallo <b>{name}</b></>
+}
+
+export const { TranslationProvider, useTranslations, Translate } = new Translation(en)
