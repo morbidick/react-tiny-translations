7d7ae928f8e48027ddc13bf4e8de43e533d30df5
Lukas Mohrbacher <lukas.mohrbacher@diconium.com>
2019-12-15 23:14:38 +0100

    add comments to app

--- example/app.tsx
+++ example/app.tsx
@@ -1,16 +1,19 @@
 import React, { FunctionComponent } from "react";
+
+// import translations from one central place in your app (needed to share types)
 import { TranslationProvider, Translate, useTranslations, de } from "./translations";
 
-const Hooks: FunctionComponent = () => {
+// Translations can be used as a component
+const TranslateComponent: FunctionComponent = () => {
     return (
         <TranslationProvider value={de}>
             <Translate key="helloName" name="Sagar" />
-            <ClassComponent />
         </TranslationProvider>
     )
 }
 
-const ClassComponent: FunctionComponent = () => {
+// or with hooks
+const TranslateHook: FunctionComponent = () => {
     const t = useTranslations()
     return <>{t.helloName({name: 'user'})}</>
 }
